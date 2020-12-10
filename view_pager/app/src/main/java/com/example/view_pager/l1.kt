490c5b59b6f8a4b9251022ca2f8507a9dbc1132b
MarkB-Mugoh <markbmugo@gmail.com>
2020-12-10 10:59:02 +0300

    First commit

---
+++ view_pager/app/src/main/java/com/example/view_pager/l1.kt
@@ -0,0 +1,12 @@
+package com.example.view_pager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class l1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_l1)
+    }
+}
